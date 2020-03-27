62ffcc7e5ee87b0e9d08335231a2fa80b42a8fd2
Gus Austin <gusaus@gmail.com>
2020-03-27 12:06:38 -0700

    removing unsupported plugins

--- wp-content/plugins/radio-station/js/radio-station-admin.js
+++
@@ -1,5 +0,0 @@
-/* --------------------------- */
-/* Radio Station Admin Scripts */
-/* --------------------------- */
-/* note: admin scripts are currently enqueued using wp_add_inline_script */
-/* this file is necessary to ensure they are printed in the right place */
--- wp-content/plugins/the-events-calendar/src/Tribe/REST/V1/Validator/Interface.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-
-interface Tribe__Events__REST__V1__Validator__Interface  extends Tribe__Events__Validator__Interface {
-
-}
