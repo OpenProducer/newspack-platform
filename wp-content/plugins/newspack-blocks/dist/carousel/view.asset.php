9ef6bfe47b8a31a5b12617756bd09fc6d95a2324
Gus Austin <gusaus@gmail.com>
2024-08-26 21:11:15 +0000

    updates via dashboard

--- wp-content/plugins/newspack-blocks/dist/carousel/view.asset.php
+++ wp-content/plugins/newspack-blocks/dist/carousel/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-a11y', 'wp-dom-ready', 'wp-escape-html', 'wp-i18n'), 'version' => '272f10765eb00ae597bb');
+<?php return array('dependencies' => array('wp-a11y', 'wp-dom-ready', 'wp-escape-html', 'wp-i18n'), 'version' => '544e9f6577938e1e5c95');
