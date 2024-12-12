9d62e873629bf37b49c00e0a20d1edc67dba2bd3
Gus Austin <gusaus@gmail.com>
2024-12-11 16:23:33 -0800

    newspack theme updates

--- wp-content/themes/newspack-block-theme/vendor/composer/autoload_classmap.php
+++ wp-content/themes/newspack-block-theme/vendor/composer/autoload_classmap.php
@@ -9,4 +9,6 @@ return array(
     'Composer\\InstalledVersions' => $vendorDir . '/composer/InstalledVersions.php',
     'Newspack_Block_Theme\\Core' => $baseDir . '/includes/class-core.php',
     'Newspack_Block_Theme\\Jetpack' => $baseDir . '/includes/class-jetpack.php',
+    'Newspack_Block_Theme\\Patterns' => $baseDir . '/includes/class-patterns.php',
+    'Newspack_Block_Theme\\Subtitle_Block' => $baseDir . '/includes/blocks/subtitle-block/class-subtitle-block.php',
 );
